af62a0f3feab4e2272e6d99a288493198bffb31b
ZakariyeAden <zakariye5417@gmail.com>
2023-05-22 13:57:06 -0500

    update

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 console.log('Hello World');
+
+// *****************
+// Object Literals
+// *****************
+let card0 = {
+  suit:'Hearts',
+  number: 2
+}
+let card1 = {
+  suit:'AceSpades',
+  number: 1
+}
+function isRed(cardToCheck){
+  console.log('In isRed', cardToCheck);
+
+  if(cardToCheck.suit === 'Hearts' || cardToCheck.suit === 'Diamond'){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isRed(card0));
+console.log(isRed(card1));
+
+// ***********************************
+// Complete the Codecademy challenege
+// ***********************************
