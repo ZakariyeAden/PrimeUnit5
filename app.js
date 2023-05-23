1418302a56ff958f5263d31e8e0b340e5fdad2d8
ZakariyeAden <zakariye5417@gmail.com>
2023-05-23 10:59:48 -0500

    practical teamwork advice

--- app.js
+++ app.js
@@ -27,3 +27,25 @@ console.log(isRed(card1));
 // ***********************************
 // Complete the Codecademy challenege
 // ***********************************
+
+
+
+// ***********************************
+// PRACTICAL TEAMWORK: GROWTH MINDSET
+// ***********************************
+
+// Continous learning
+// What is Growth mindset? Intelligence is Developed through Effort and Practice.
+// How to engage in problem solving behavior.
+// Perservance when challenged
+// Setbacks and mistakes are a natural part of learning. It’s what happens when we take on valuable challenges.
+// Growth Mindset: Accepts challenge and build new skill sets.
+// Ask Questions, request feedback, and apply lessons learned.
+// Learn by effort, mentors, dedication, and learning. Not everybody is the same.
+
+
+
+// Must have confindence in yourself.Be able to speak clearly and confidently.
+// Need mindset, commiting to doing, tactical decision are recording your daily actions.
+// What you need to have confindence speak more slowly, say fewer words, smile more, Focus on the other person
+
